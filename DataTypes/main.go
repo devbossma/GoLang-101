b72f16398a086602172e4b54a602770cc9e34c4b
devbossma <ysaber201@outlook.com>
2026-02-17 21:07:40 +0000

    Adding more golang primitive types

--- DataTypes/main.go
+++ DataTypes/main.go
@@ -4,6 +4,25 @@ import "fmt"
 
 func main(){
 
+	/* 
+	** Go's basic (primitive) types are the building blocks for all other data structures in the language. They are categorized into three main groups: Boolean, Numeric, and String types. 
+	** The specific primitive types are listed below: 
+	** 
+	** ## bool: Represents a boolean value, which is either true or false.
+	** ## Numeric Types:
+	** ### > Integers:
+	**   	* Signed integers: int8, int16, int32, int64, and int. The plain int type is generally recommended unless a specific size is required; its size is platform-dependent (either 32 or 64 bits).
+	**   	* Unsigned integers: uint8, uint16, uint32, uint64, and uint.
+	** ### > Aliases: byte is an alias for uint8, and rune is an alias for int32 (used to represent a Unicode code point).
+	** ### > Other: uintptr is an unsigned integer type large enough to store the uninterpreted bits of a pointer value.
+	**
+	** ### > Floating-point numbers: float32 and float64.
+	**
+	** ## > Complex numbers: complex64 and complex128.
+	** ## > string: A sequence of bytes, conventionally but not necessarily representing UTF-8 encoded text. String values are immutable in Go. 
+	** 
+	*/
+
 	// Section1: Integers in Go
 	// Declaration: var var_nam type
 
@@ -23,14 +42,14 @@ func main(){
 	i32 = 2147483647
 	i64 = -3987509348750913
 
-	// A: Unsigned Integers
+	// B: Unsigned Integers
 
 	var u uint // uint: Platform-dependent (32 or 64 bits).
 	var u8 uint8 // uint8 (alias byte): 0 to 255
 	var u16 uint16 // uint16: 0 to 65,535
 	var u32 uint32 // uint32: 0 to 4,294,967,295
 	var u64 uint64 // uint64: 0 to 18,446,744,073,709,551,615
-	var uintlarge uintptr    //: An unsigned integer type large enough to store the bit pattern of any pointer. 
+	// var uintlarge uintptr    //: An unsigned integer type large enough to store the bit pattern of any pointer. 
 	// Value Assignment
 	u 	= 255
 	u8  = 255
@@ -42,5 +61,57 @@ func main(){
 	fmt.Println("USigned Integers", u, u8, u16, u32, u64)
 	
 
+	// Section2: Float in Go
 
+	//  A: Float32: Used for less precise calculations
+	var f32 float32 = 10.06 // Approximately -3.4e+38 to 3.4e+38
+	var LP float32 = 10123456789012345
+	fmt.Println("Float32: ", f32)
+	//  B: Float64: Used for more precise calculations
+	var f64 float64 = 654.80876760860860 // Approximately -1.7e+308 to +1.7e+308
+	var HP float64 = 10123456789012345
+	fmt.Println("Float64: ", f64)
+
+	// Low Precesion Vs Hight Precesion
+	fmt.Println("LP", LP) // LP 1.0123457e+16
+	fmt.Println("HP", HP) // HP 1.0123456789012344e+16
+	
+	// Section3: Boolean Types
+
+	var on bool = true
+	var off bool = false
+	fmt.Println("is Active", on)
+	fmt.Println("is Active", off)
+
+
+	// Section4: Complex Data Type
+	var CN1 complex128 = complex(5, 10)
+	var CN2 complex64 = complex(2, 7)
+
+	fmt.Println("CN1", CN1) // CN1 (5+10i)
+	fmt.Println("CN2", CN2) // CN2 (2+7i)
+
+	// Using a literal: c := 3 + 4i. An imaginary literal (like 4i) denotes a complex number with a zero real component. 
+	c1 := 2 + 3i
+    c2 := 4 + 5i
+    fmt.Println("Addition:", c1 + c2)       // Output: Addition: (6+8i)
+    fmt.Println("Multiplication:", c1 * c2) // Output: Multiplication: (-7+22i)
+    fmt.Println("Real part:", real(c1 * c2))  // Output: Real part: -7
+    fmt.Println("Imaginary part:", imag(c1 * c2)) // Output: Imaginary part: 22
+
+	// Section5: String Data Type
+	var name string = "Yassine"
+
+	// formatting but not printing to the console
+	var greating string = fmt.Sprintf("Hello... %s!", name)
+    fmt.Println(greating)
+	// formatting and printing to the console but no new line
+	// fmt.Printf("Hello... %s!", name) // no new line
+
+	// Bonus of using a short hand declaratin and assignments
+	// (:=) : is the short variable declaration operator. It is a shorthand for declaring and initializing a variable in a single statement, with the compiler automatically inferring the variable's type based on the assigned value. 
+
+	age := 32
+	// age = "Yassine" // cannot use "Yassine" (untyped string constant) as int value in assignment
+	fmt.Println("My Age is not", age)
 }
